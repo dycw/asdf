48748df95f37bd323fee41e3e38e2a560b29edae
Derek Wan <d.wan@icloud.com>
2023-05-12 19:16:02 +0800

    Finish lesson 5 (npm) (#6)

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const http = require("http");
 const fs = require("fs");
+const _ = require("lodash");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+  const num = _.random(0, 20);
+  console.log(num);
+
+  const greet = _.once(() => {
+    console.log("hello");
+  });
+  greet();
+  greet();
 
   res.setHeader("Content-Type", "text/html");
 
@@ -16,7 +24,7 @@ const server = http.createServer((req, res) => {
       path += "about.html";
       res.statusCode = 200;
       break;
-    case "/about-me":
+    case "/about-us":
       res.statusCode = 301;
       res.setHeader("Location", "/about");
       res.end();
