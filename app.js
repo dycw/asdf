2a735879a62782be0f13f7f1cf837475bd0d775c
Derek Wan <d.wan@icloud.com>
2023-05-13 09:08:18 +0800

    Finish lesson 7 (view engines)

--- app.js
+++ app.js
@@ -2,20 +2,27 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.listen(8000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    { title: "Yoshi finds eggs", snippet: "Lorem ipsum dolor sit amet" },
+    { title: "Mario finds stars", snippet: "Lorem ipsum dolor sit amet" },
+    { title: "How to defeat bowser", snippet: "Lorem ipsum dolor sit amet" },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.redirect("create", { title: "Create a new Blog" });
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
