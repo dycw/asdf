db84edf3688a80ff8c9c8918063519a542a5e387
Derek Wan <d.wan@icloud.com>
2023-05-13 10:41:18 +0800

    Finish lesson 10 (get, post & delete requests)

--- app.js
+++ app.js
@@ -14,16 +14,12 @@ mongoose
     console.log("connected to db");
     app.listen(8000);
   })
-  .catch((err) => {
-    console.log(err);
-  });
+  .catch((err) => console.log(err));
 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
-app.use(morgan("dev"));
-
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
@@ -34,19 +30,41 @@ app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
-app.get("/blogs", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("index", { title: "All blogs", blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+  blog
+    .save()
+    .then((result) => res.redirect("/blogs"))
+    .catch((err) => console.log(err));
 });
 
 app.get("/blogs/create", (req, res) => {
-  res.redirect("create", { title: "Create a new Blog" });
+  res.render("create", { title: "Create a new Blog" });
+});
+
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) =>
+      res.render("details", { blog: result, title: "Blog Details" }),
+    )
+    .catch((err) => console.log(err));
+});
+
+app.delete("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then((result) => res.json({ redirect: "/blogs" }))
+    .catch((err) => console.log(err));
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) =>
+      res.render("index", { title: "All blogs", blogs: result }),
+    )
+    .catch((err) => console.log(err));
 });
 
 app.use((req, res) => {
